fa69919096eff9bd52052157813ce77eb53c2304
Marco Biasini <mvbiasini@gmail.com>
2014-11-02 16:49:57 +0100

    add help text in case no verb is provided

--- mug/main.py
+++ mug/main.py
@@ -2,7 +2,19 @@
 import sys
 from mug import verbs
 
+HELP='''\
+usage: mug <verb> [<args>]
+
+Supported verbs are:
+
+    status           Show working tree status
+    commit           Record changes to the repository
+    add              Add file contents to the index
+'''
 def main(argv=sys.argv):
+    if len(argv) == 1:
+        print HELP
+        sys.exit(-1)
     verbs.execute(argv[1:])
 
 if __name__ == '__main__':
